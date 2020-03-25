72743b2d2d22089e8d325965ae71dff29834643a
Matt Readout <matt.readout@gmail.com>
2020-03-25 16:48:04 -0700

    Add get_ and add_players to created updated game with players

--- lib/game.ex
+++ lib/game.ex
@@ -1,5 +1,18 @@
 defmodule TicTacToe.Game do
+  alias TicTacToe.CLI
+  alias TicTacToe.Player
+  alias TicTacToe.Game
 
-  defstruct [:board] 
+  defstruct [:board, players: []]
+
+  def get_players(input \\ CLI) do
+    player1 = input.get_player_name("X")
+    player2 = input.get_player_name("O")
+    [%Player{name: player1}, %Player{name: player2}]
+  end
+
+  def add_players(game, input \\ CLI) do
+    Map.replace!(game, :players, Game.get_players(input))
+  end
 
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -5,8 +5,15 @@ defmodule TicTacToe.GameTest do
 
   test "game has a board" do
     game = %Game{board: Board.new(3)}
-
     assert length(game.board.spaces) == 9
+  end
+
+  test "game has two players" do
+    game = %Game{}
+    assert length(game.players) == 0
+
+    updated_game = Game.add_players(game, MockInput)
+    assert length(updated_game.players) == 2
   end
 
 end

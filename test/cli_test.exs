943bb646cc960134cd0b051167d9f16c582bd2b9
Matt Readout <matt.readout@gmail.com>
2020-03-25 16:47:11 -0700

    Add prompt for player names

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -6,4 +6,10 @@ defmodule TicTacToe.CLITest do
   test "console prints welcome message" do
     assert capture_io(fn -> CLI.welcome() end) =~ "Welcome"
   end
+
+  test "gets player name" do
+    assert capture_io([input: "Player One", capture_prompt: false], fn ->
+      IO.write CLI.get_player_name("X", &MockInput.gets/1)
+    end) == "Player One"
+  end
 end

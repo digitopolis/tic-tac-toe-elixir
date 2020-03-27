c73fef9d4b76f69f274e3ce74027da0673746686
Matt Readout <matt.readout@gmail.com>
2020-03-27 10:10:19 -0700

    Add Game.make_move/3 to update board with player's selected move

--- lib/game.ex
+++ lib/game.ex
@@ -2,6 +2,7 @@ defmodule TicTacToe.Game do
   alias TicTacToe.CLI
   alias TicTacToe.Player
   alias TicTacToe.Game
+  alias TicTacToe.Board
 
   defstruct [:board, players: []]
 
@@ -13,6 +14,19 @@ defmodule TicTacToe.Game do
 
   def add_players(game, input \\ CLI) do
     Map.replace!(game, :players, Game.get_players(input))
+  end
+
+  def make_move(game, move, input \\ CLI) do
+    [ current_player | _tail ] = game.players
+    cond do
+      !Board.space_is_available(game.board.spaces, move) ->
+        input.print "That space isn't available, please choose an open space between 1 and #{length(game.board.spaces)}"
+        new_move = input.get_player_move(current_player.name)
+        make_move(game, new_move, input)
+      Board.space_is_available(game.board.spaces, move) ->
+        new_board = Board.update_at(game.board.spaces, move, current_player.marker)
+        update_in(game, [Access.key(:board), Access.key(:spaces)], &(&1 = new_board))
+    end
   end
 
 end

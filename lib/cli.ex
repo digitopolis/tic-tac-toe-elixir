059e184a07c72146523e5504568136b7850ad772
Matt Readout <matt.readout@gmail.com>
2020-03-27 11:28:10 -0700

    Add CLI.display_board/1 to print a formatted game board

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,4 +1,6 @@
 defmodule TicTacToe.CLI do
+  alias TicTacToe.Board
+  alias TicTacToe.CLI
 
   def welcome() do
     IO.puts "Welcome to Tic Tac Toe!"
@@ -6,6 +8,27 @@ defmodule TicTacToe.CLI do
 
   def print(message) do
     IO.puts message
+  end
+
+  def get_display_list(spaces) do
+    for x <- [1, 2, 3, 4, 5, 6, 7, 8, 9] do
+      if Board.space_is_available(spaces, x) do
+        x
+      else
+        Enum.at(spaces, x - 1)
+      end
+    end
+  end
+
+  def display_board(spaces) do
+    display_spaces = CLI.get_display_list(spaces)
+    IO.write """
+       #{Enum.at(display_spaces, 0)}  |  #{Enum.at(display_spaces, 1)}  |  #{Enum.at(display_spaces, 2)}
+      ----+-----+----
+       #{Enum.at(display_spaces, 3)}  |  #{Enum.at(display_spaces, 4)}  |  #{Enum.at(display_spaces, 5)}
+      ----+-----+----
+       #{Enum.at(display_spaces, 6)}  |  #{Enum.at(display_spaces, 7)}  |  #{Enum.at(display_spaces, 8)}
+      """
   end
 
   def get_player_name(marker, gets \\ &IO.gets/1) do

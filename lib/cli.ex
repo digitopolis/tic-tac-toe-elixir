3d132de9f1d3e7c1eba21a8bd2d07c45e516e754
Matt Readout <matt.readout@gmail.com>
2020-03-27 10:09:17 -0700

    Remove new line characters from string inputs and return int for move

--- lib/cli.ex
+++ lib/cli.ex
@@ -4,11 +4,18 @@ defmodule TicTacToe.CLI do
     IO.puts "Welcome to Tic Tac Toe!"
   end
 
+  def print(message) do
+    IO.puts message
+  end
+
   def get_player_name(marker, gets \\ &IO.gets/1) do
     gets.("Player #{marker}, please enter your name:\n")
+      |> String.trim()
   end
 
   def get_player_move(name, gets \\ &IO.gets/1) do
     gets.("#{name}, please select a space:\n")
+      |> String.trim()
+      |> String.to_integer()
   end
 end

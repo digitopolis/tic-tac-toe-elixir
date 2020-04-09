5ec261b95f8ee22b553c5b10cb744808c76fe6c0
Matt Readout <matt.readout@gmail.com>
2020-04-09 14:44:13 -0700

    Add methods to update or create save in DB

--- lib/database.ex
+++ lib/database.ex
@@ -1,10 +1,11 @@
 defmodule TicTacToe.Database do
   alias TicTacToe.User
+  alias TicTacToe.Save
   alias TicTacToe.Repo
   alias TicTacToe.CLI
 
   def get_player(name) do
-    TicTacToe.User |> TicTacToe.Repo.get_by( name: name )
+    TicTacToe.User |> TicTacToe.Repo.get_by( name: name ) |> TicTacToe.Repo.preload(:save)
   end
 
   def new_player(name) do
@@ -22,5 +23,23 @@ defmodule TicTacToe.Database do
   def update_user(changeset) do
     {:ok, user} = Repo.update(changeset)
     CLI.print "Congratulations #{user.name}, that brings your win total to #{user.wins}"
+  end
+
+  def update_save(changeset) do
+    { :ok, _save } = Repo.update(changeset)
+    CLI.print "Game saved"
+  end
+
+  def save_game(name, spaces) do
+    user = get_player(name)
+    save = %Save{ user_id: user.id, spaces: spaces }
+    cond do
+      user.save ->
+        Save.changeset(user.save, %{ spaces: spaces })
+          |> update_save
+      true ->
+        { :ok, _save } = TicTacToe.Repo.insert save
+        CLI.print "Game saved"
+    end
   end
 end
